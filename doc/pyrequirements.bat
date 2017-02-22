9f75b4f1b53855e807049e5f8d87646ebf034bde
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-02-22 20:37:32 -0300

    Documentation

---
+++ doc/pyrequirements.bat
@@ -0,0 +1,10 @@
+C:\python27\python.exe c:\python27\tools\scripts\win_add2path.py
+cd C:\
+C:\python27\python.exe get-pip.py
+C:\python27\python.exe c:\python27\tools\scripts\win_add2path.py
+C:\python27\python.exe -m pip install pillow
+C:\python27\python.exe -m pip install lxml-3.7.3-cp27-cp27m-win32.whl
+C:\python27\python.exe -m pip install lxml-3.7.3-cp27-cp27m-win_amd64.whl
+C:\python27\python.exe -m pip install scrapy
+echo Installation complete. You should run the script by executing 'run.bat'.
+pause
---
+++ run.bat
@@ -0,0 +1,2 @@
+c:\\python27\python run_spider.py
+pause
